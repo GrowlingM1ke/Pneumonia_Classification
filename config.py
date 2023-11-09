6941ed1ab0b9fe44f814549669f679e2dae7815c
milange <milange@iu.edu>
2023-11-09 17:48:33 -0500

    removed unecessary stuff from UNET project.

--- config.py
+++ config.py
@@ -15,15 +15,10 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 # determine if we will be pinning memory during data loading
 PIN_MEMORY = True if DEVICE == "cuda" else False
 
-# define the number of channels in the input, number of classes,
-# and number of levels in the U-Net model
-NUM_CHANNELS = 1
-NUM_CLASSES = 1
-NUM_LEVELS = 3
 # initialize learning rate, number of epochs to train for, and the
 # batch size
 INIT_LR = 0.001
-NUM_EPOCHS = 50
+NUM_EPOCHS = 3
 BATCH_SIZE = 20
 # define the input image dimensions
 INPUT_IMAGE_WIDTH = 512
